b2d5286c35fae6743c57b6575811cb4d3e99ee0b
Anhelina Volkova <angel.love5052@gmail.com>
2026-08-09 02:31:12 +0200

    changed

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const loadButton = document.querySelector('.loadMoreButton');
 
 //началльные параметры
 hideLoadBtn();
-let pageNumber = 1;
+let pageNumber;
 
 //Обработка сабмита
 searchForm.addEventListener("submit", event => {
@@ -33,108 +33,97 @@ loadButton.addEventListener("click", event => {
     event.preventDefault();
     pageNumber += 1;
     loadGallery(pageNumber);
+    hideLoadBtn();
 });
 
+
+const loadGallery = async (pageNumber) => {
+    /*
+    Check Value (EMPTY_FIELD)
+    Serch photo (NO_IMAGES)
+    Page Scroll
+        1. totalHits < 15
+            весь результат на первой страничке, дальше не грузим
+        2. все результаты показаны (MAX_PAGES)
+        3. грузим дальше с прокруткой
+    Gallery Render (data.hits)
+     */
+    try {
+        showLoader();
+        hideLoadBtn();
+        const currentQuery = await checkValue(searchInput.value);
+        const photoDatas = await searchPhoto(currentQuery, pageNumber);
+        const resultPhotos = await scrollPhotos(photoDatas, pageNumber);
+        //galleryRender(resultPhotos);
+    }
+    catch (error) {
+        switch (error.code) {
+            case "EMPTY_FIELD":
+                iziToast.error({
+                    message: "Field can't be empty."
+                });
+                console.error(error);
+                break;
+            
+            case "NO_IMAGES":
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                });
+                console.error(error);
+                break;
+            
+            case "MAX_PAGES":
+                iziToast.error({
+                    message: "We're sorry, but you've reached the end of search results."
+                });
+                console.error(error);
+                break;
+            
+            default:
+                iziToast.error({
+                    message: "Something went wrong. Please try again later."
+                });
+                console.error(error);
+                break;
+        }
+    }
+    finally {
+        hideLoader();
+    };
+};
+
+const scrollPhotos = async(data, page) => {
+    const totalHitsValue = data.totalHits;
+    const imagesPerPage = data.per_page;
+    const maxPages = Math.floor(totalHitsValue / imagesPerPage);
+    if (page === 1) {
+        galleryRender(data.hits);
+        showLoadBtn();
+    } else if (maxPages < page && page!=1) {
+        hideLoadBtn();
+        const error = new Error();
+        error.code = 'MAX_PAGES';
+        throw error;
+    } else {
+        galleryRender(data.hits);
+        const galleryItem = document.querySelector(".gallery-link");
+        const galleryItemParams = galleryItem.getBoundingClientRect();
+        const itemHeight = galleryItemParams.height;
+        const scrollValue = itemHeight * 2;
+        window.scrollBy({
+            top: scrollValue,
+            behavior: "smooth",
+        });
+        showLoadBtn();
+    }
+};
+
 //Обработка значения инпута (пустая или нет)
-function checkValue(value) {
+const checkValue = async(value) => {
     const trimedValue = value.trim();
     if (trimedValue === '') {
         const error = new Error();
         error.code = 'EMPTY_FIELD';
         throw error;
     } return trimedValue;
-}
-
-//Загрузка галереи с значением из инпута
-function loadGallery(pageNumber) {
-    const query = checkValue(searchInput.value);
-    showGallery(query, pageNumber)
-        .then()
-        .catch(error => {
-            if (error.code === 'MAX_PAGES') {
-                iziToast.error({
-                    message: "We're sorry, but you've reached the end of search results."
-                });
-                console.error(error);
-            } else {
-                iziToast.error({
-                    message: "Something went wrong. Please try again later."
-                });
-                console.error(error);
-            }
-        });
-}
-
-//Проверка основных функций (рендер и поиск фото) на ошибки
-const mainFunction = async (query, pageNumber) => {
-    try {
-        const photos = await searchPhoto(query, pageNumber);
-        galleryRender(photos.hits);
-        return photos;
-    } catch (error) {
-        switch (error.code) {
-            case "EMPTY_FIELD":
-                iziToast.error({
-                    message: "Field can't be empty."
-                });
-                break;
-            
-            case 'NO_IMAGES':
-                iziToast.error({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                });
-                break;
-
-            default:
-                iziToast.error({
-                    message: "Something went wrong. Please try again later."
-                });
-                console.error(error);
-        }
-    } finally {
-        hideLoader();
-    }
 };
-
-//Добавляем зависимость галереи от страницы и колличества результатов
-const showGallery = async(query, page) => { 
-    showLoader();
-    hideLoadBtn();
-    const galleryData = await mainFunction(query, page)
-        .then(data => {
-            const totalImages = galleryData.totalHits;
-            const imagesPerPage = galleryData.per_page;
-            const maxPages = Math.floor(totalImages / 15);
-            if (maxPages <= page || totalHits < 15) {
-                hideLoadBtn();
-                const error = new Error();
-                error.code = 'MAX_PAGES';
-                throw error;
-            } else if (page>1) {
-                const galleryItem = document.querySelector(".gallery-link");
-                const galleryItemParams = galleryItem.getBoundingClientRect();
-                const itemHeight = galleryItemParams.height;
-                const scrollValue = itemHeight * 2;
-                window.scrollBy({
-                    top: scrollValue,
-                    behavior: "smooth",
-                });
-            }else {
-                showLoadBtn();
-            }
-        })
-        .catch(error => {
-            iziToast.error({
-                message: "Something went wrong. Please try again later."
-            });
-            console.error(error);
-        });
-}
-
-
-
-
-
-
-
-
